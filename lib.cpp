ea23c7d92c2bb35207abfd05149cf3999d1ea05b
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-12-01 19:22:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include "lib.h"
 
+bool NumeroPrimo (int n, int k)
+{
+    n--;
+    if (n>1)
+    {
+        if (k%n==0)
+        {
+            return false;
+        }
+        else
+        {
+            NumeroPrimo(n,k);
+        }
+    }
+    else
+    {
+        return true;
+    }
+}
